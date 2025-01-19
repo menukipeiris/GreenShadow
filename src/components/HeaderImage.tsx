763cc7fb6c44ea459a74cbcfb35be465cb9ca171
menukipeiris <menukimpeiris@gmail.com>
2025-01-19 23:05:35 +0530

    Added Login page image

--- src/components/HeaderImage.tsx
+++ src/components/HeaderImage.tsx
@@ -3,8 +3,10 @@ export function HeaderImage() {
     <div className="font-[sans-serif] relative">
       <div className="h-[250px] font-[sans-serif]">
         <img
-          src="https://www.farmers.gov/sites/default/files/img/farmersgov-1440x400-crop-acreage-reporting.jpg"
-          alt="Banner Image"
+          // src="https://www.farmers.gov/sites/default/files/img/farmersgov-1440x400-crop-acreage-reporting.jpg"
+          //   src="https://www.google.com/imgres?imgurl=https://w7.pngwing.com/pngs/253/592/png-transparent-farmers-using-smart-device-for-crop-monitoring-illustration.png&tbnid=UFyP8v4D2ZP5PM&vet=1&imgrefurl=https://www.pngwing.com/en/free-png-aahak&docid=23Xu7JTvyNVooM&w=920&h=614&hl=en&source=sh/x/im/m1/1&kgs=55e5b69ba9570c1e"
+            src="src/assets/banner.png"
+            alt="Banner Image"
           className="w-full h-full object-cover"
         />
       </div>
