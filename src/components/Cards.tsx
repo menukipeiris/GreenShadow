46503e951661efc5c213f5183fcb2db110764770
menukipeiris <menukimpeiris@gmail.com>
2025-01-19 22:35:53 +0530

    Changed cards colour

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -6,7 +6,7 @@ export function Cards() {
     <div className="container p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <motion.div
-          className="bg-blue-500 text-white p-6 rounded-lg shadow-lg h-50 "
+          className="bg-green-700 text-white p-6 rounded-lg shadow-lg h-50 "
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 0.1, ease: easeIn }}
